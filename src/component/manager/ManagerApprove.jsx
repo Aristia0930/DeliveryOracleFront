88da97e0aec68b73b6e891ea913b7c34b2afcf9b
DESKTOP-USS0OE9\KOSTA <ablesdm@naver.com>
2024-06-13 17:31:11 +0900

    관리자 승인 버튼 까지 그리고 백앤드만 남음

--- src/component/manager/ManagerApprove.jsx
+++ src/component/manager/ManagerApprove.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ManagerApproveList from './ManagerApproveList';
+
 const ManagerApprove = () => {
 
     //요청 받아서 목록 받아오고 이걸전개 한다 
     return (
         <div>
             <h1>업체 승인 목록</h1>
-            <ManagerApproveList />    
+            <ManagerApproveList /> 
         </div>
     );
 };
