471e981c5b6c35a8439bb42f60f17d3559962dd1
itrecipe <itrecipe95@gmail.com>
2024-07-04 10:54:16 +0900

    Revert "Merge remote-tracking branch 'origin/temp'"
    This reverts commit a9d87ba5eafe8cbbfa09496842c651c75ab9889f, reversing
    changes made to 06690dae7f613e23a0af79dfdd022f5e4ffe1320.

--- src/component/common/TabMenu.jsx
+++ src/component/common/TabMenu.jsx
@@ -1,60 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useContext } from "react";
-import { AdminFlagContext } from "../../flag/Flag.jsx";
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 const TabMenu = () => {
-
-  const { user_x, setRole,setX, user_y, setY ,userId,setUserId,user,setUser,setUserDate} = useContext(AdminFlagContext);
-  const [location, setLocation] = useState({ latitude: null, longitude: null });
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                setLocation({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                });
-                setX(position.coords.longitude);
-                setY(position.coords.latitude);
-                setError(null);
-                console.log("위치",position.coords.longitude)
-            },
-            (error) => {
-                setError(error.message);
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 5000,
-                maximumAge: 0,
-            }
-        );
-    } else {
-        setError('Geolocation is not supported by this browser.');
-    }
-}, []);
-
   return (
     <ul className="nav nav-tabs nav-menu">
       <li className="nav-item">
-        <Link className="nav-link active" to="/UserMenuCaList" state={{ ca: 1, y: location.latitude, x: location.longitude }}>한식</Link>
+        <a className="nav-link active" href="#">한식</a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/UserMenuCaList" state={{ ca: 2, y: location.latitude, x: location.longitude }}>중식</Link>
+        <a className="nav-link" href="#">중식</a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/UserMenuCaList" state={{ ca: 3, y: location.latitude, x: location.longitude }}>일식</Link>
+        <a className="nav-link" href="#">일식</a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/UserMenuCaList" state={{ ca: 4, y: location.latitude, x: location.longitude }}>치킨</Link>
+        <a className="nav-link" href="#">치킨</a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/UserMenuCaList" state={{ ca: 5, y: location.latitude, x: location.longitude }}>피자</Link>
+        <a className="nav-link" href="#">피자</a>
       </li>
       <li className="nav-item">
-        <Link className="nav-link" to="/UserMenuCaList" state={{ ca: 6, y: location.latitude, x: location.longitude }}>AI추천</Link>
+        <a className="nav-link" href="#">분식</a>
       </li>
     </ul>
   );
