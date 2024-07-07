d852e40f7086005d671368cbb7950b159f35da55
ari <kjk9858@gmail.com>
2024-07-08 00:43:02 +0900

    파일위치 수정

---
+++ src/page/riderPage/RiderMain.jsx
@@ -0,0 +1,98 @@
+import React, { useEffect,useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import Header from '../../component/rider/headside/Header.jsx';
+import Sidebar from '../../component/rider/headside/Sidebar.jsx';
+import { useContext } from "react";
+import { AdminFlagContext } from "../../flag/Flag.jsx";
+import axios from 'axios';
+import Footer from '../../component/common/Footer.jsx';
+
+const RiderMain = () => {
+    const {setRole,user,setUser,userId,setUserId,shopId,setShopId,user_x,setX,user_y,setY}=useContext(AdminFlagContext)
+    //jwt 아이디 저장한다.
+    const navigate = useNavigate();    
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        // if (!user){
+        //     alert("로그인해주세요")
+        //     navigate("/")
+        // }
+        const fetchUserInfo = async () => {
+
+
+            const token = user
+            console.log(token)
+            console.log("jwt 불러오는ㅈ")
+            try {
+                const response = await axios.get('http://localhost:8080/api/api/userinfo', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                console.log(response.data);
+                // console.log(response.data.user_id);
+                // setUserDate(response.data.user_id)
+                //유저아이디를 플래그에 저장
+                setUserId(response.data.user_id)
+                setRole(response.data.authList[0].auth)
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        //라이더 위치 조회
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+
+                setX(position.coords.longitude)
+                setY(position.coords.latitude)
+                setError(null);
+              },
+              (error) => {
+                setError(error.message);
+              },
+              {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0,
+              }
+            );
+          } else {
+            setError('Geolocation is not supported by this browser.');
+          }
+
+        fetchUserInfo();
+    }, []);
+     
+    //콜받기에서는
+    //주문 오더와 상점 정보를 기반으로 나와의 거리 상점과의 거리 값과 배달 비 받아온다.
+    //오더 주문no,시킨사람아이디 이렇게 4개 받아온다. 
+    //배달비도 보여준다.(서비스단에서) 계산 돌리자
+
+    //주문내역확인
+    //서로간의 거리와
+    //배달비 를 받는다.
+
+
+    return (
+        <div>
+         <Header />
+            <Container fluid>
+                <Row>
+                    <Col xs={2} id="sidebar-wrapper">
+                        <Sidebar/>
+                    </Col>
+                    <Col xs={10} id="page-content-wrapper">
+                    <h1>메인</h1>
+ 
+
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
+        </div>
+    );
+};
+
+export default RiderMain;
--- src/routerPath/Path.jsx
+++ src/routerPath/Path.jsx
@@ -1,58 +1,58 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from '../component/main/Main';
-import UserMain from '../component/user/UserMain';
-import ShopMain from '../component/shop/ShopMain';
-import UserLogin from '../component/user/UserLogin';
-import UserJoin from '../component/user/UserJoin';
-import ShopJoin from '../component/shop/ShopJoin';
-import ManagerMain from '../component/manager/ManagerMain';
-import ManagerApprove from '../component/manager/ManagerApprove';
-import ManagerOrderReceipt from '../component/manager/ManagerOrderReceipt';
-import ManagerRevenue from '../component/manager/ManagerRevenue';
-import ShopMenu from '../component/shop/ShopMenu';
-import ShopMenuRs from '../component/shop/ShopMenuRs';
-import ShopMenuList from '../component/shop/ShopMenuList';
-import ShopMenuedit from '../component/shop/ShopMenuedit';
-import UserMenuCaList from '../component/user/UserMenuCaList';
-import UserShopDetail from '../component/user/UserShopDetail';
-import ShopOrder from '../component/shop/ShopOrder';
-import RiderMain from '../component/rider/RiderMain';
-import RiderCall from '../component/rider/RiderCall';
-import RiderOrder from '../component/rider/RiderOrder';
+import UserMain from '../page/userPage/UserMain';
+import ShopMain from '../page/shopPage/ShopMain';
+import UserLogin from '../page/userPage/UserLogin';
+import UserJoin from '../page/userPage/UserJoin';
+import ShopJoin from '../page/shopPage/ShopJoin';
+import ManagerMain from '../page/managerPage/ManagerMain';
+import ManagerApprove from '../page/managerPage/ManagerApprove';
+import ManagerOrderReceipt from '../page/managerPage/ManagerOrderReceipt';
+import ManagerRevenue from '../page/managerPage/ManagerRevenue';
+import ShopMenu from '../page/shopPage/ShopMenu';
+import ShopMenuRs from '../page/shopPage/ShopMenuRs';
+// import ShopMenuList from '../component/shop/ShopMenuList';
+import ShopMenuedit from '../page/shopPage/ShopMenuedit';
+import UserMenuCaList from '../page/userPage/UserMenuCaList';
+import UserShopDetail from '../page/userPage/UserShopDetail';
+import ShopOrder from '../page/shopPage/ShopOrder';
+import RiderMain from '../page/riderPage/RiderMain';
+import RiderCall from '../page/riderPage/RiderCall';
+import RiderOrder from '../page/riderPage/RiderOrder';
 import RiderOrderList from '../component/rider/RiderOrderList';
-import RiderOrderReceipt from '../component/rider/RiderOrderReceipt';
+import RiderOrderReceipt from '../page/riderPage/RiderOrderReceipt';
 import RiderOrderReceiptList from '../component/rider/RiderOrderReceiptList';
 import RiderRechart from '../component/rider/RiderRechart';
-import RiderRevenue from '../component/rider/RiderRevenue';
-import MypageMain from '../component/mypage/MypageMain';
-import MyorderDetails from '../component/mypage/MyorderDetails';
+import RiderRevenue from '../page/riderPage/RiderRevenue';
+import MypageMain from '../page/mypage/MypageMain';
+import MyorderDetails from '../page/mypage/MyorderDetails';
 import ShopOrderReceipt from '../component/shop/ShopOrderReceipt';
-import ShopRevenue from '../component/shop/ShopRevenue';
-import MypageUserEdit from '../component/mypage/MypageUserEdit';
-import UserSearchList from '../component/user/UserSearchList';
-import MypageComments from '../component/mypage/MypageComments';
-import UserShopIntroduce from '../component/user/UserShopIntroduce';
-import ShopEdit from '../component/shop/ShopEdit';
-import UserShopComment from '../component/user/UserShopComment';
-import ShopComment from '../component/shop/ShopComment';
-import ManagerUserblock from '../component/manager/ManagerUserblock';
-import MypageReview from '../component/mypage/MypageReview';
+import ShopRevenue from '../page/shopPage/ShopRevenue';
+import MypageUserEdit from '../page/mypage/MypageUserEdit';
+import UserSearchList from '../page/userPage/UserSearchList';
+import MypageComments from '../page/mypage/MypageComments';
+import UserShopIntroduce from '../page/userPage/UserShopIntroduce';
+import ShopEdit from '../page/shopPage/ShopEdit';
+import UserShopComment from '../page/userPage/UserShopComment';
+import ShopComment from '../page/shopPage/ShopComment';
+import ManagerUserblock from '../page/managerPage/ManagerUserblock';
+import MypageReview from '../page/mypage/MypageReview';
 import ShopCommentText from '../component/shop/ShopCommentText';
-import ManagerShopleave from '../component/manager/ManagerShopleave';
-import ShopUserJoin from '../component/shop/ShopUserJoin';
-import ShopUserLogin from '../component/shop/ShopUserLogin';
-import ManagerUserJoin from '../component/manager/ManagerUserJoin';
-import ManagerUserLogin from '../component/manager/ManagerUserLogin';
-import RiderUserJoin from '../component/rider/RiderUserJoin';
-import RiderUserLogin from '../component/rider/RiderUserLogin';
-import ShopMyPage from '../component/shop/ShopMyPage';
+import ManagerShopleave from '../page/managerPage/ManagerShopleave';
+import ShopUserJoin from '../page/shopPage/ShopUserJoin';
+import ShopUserLogin from '../page/shopPage/ShopUserLogin';
+import ManagerUserJoin from '../page/managerPage/ManagerUserJoin';
+import ManagerUserLogin from '../page/managerPage/ManagerUserLogin';
+import RiderUserJoin from '../page/riderPage/RiderUserJoin';
+import RiderUserLogin from '../page/riderPage/RiderUserLogin';
+import ShopMyPage from '../page/shopPage/ShopMyPage';
 import ShopMyPagemodify from '../component/shop/ShopMyPagemodify';
-import RiderMyPage from '../component/rider/RiderMyPage';
+import RiderMyPage from '../page/riderPage/RiderMyPage';
 import RiderMyPagemodify from '../component/rider/RiderMyPagemodify';
 import ManagerMyPagemodify from '../component/manager/ManagerMyPagemodify';
-import MypageReviewEdit from '../component/mypage/MypageReviewEdit';
-import UserAiList from '../component/user/UserAiList';
+import MypageReviewEdit from '../page/mypage/MypageReviewEdit';
+import UserAiList from '../page/userPage/UserAiList';
 
 const Path = () => {
     return (
@@ -74,7 +74,7 @@ const Path = () => {
             <Route path="/ShopMain" element={<ShopMain />} />
             <Route path="/ShopMenu" element={<ShopMenu />} />
             <Route path="/ShopMenuRs" element={<ShopMenuRs />} />
-            <Route path="/ShopMenuList" element={<ShopMenuList />} />
+            {/* <Route path="/ShopMenuList" element={<ShopMenuList />} /> */}
             <Route path="/ShopMenuedit" element={<ShopMenuedit />} />
             <Route path="/ShopOrder" element={<ShopOrder />} />
             <Route path="/ShopOrderReceipt" element={<ShopOrderReceipt/>} />
@@ -90,10 +90,10 @@ const Path = () => {
             <Route path="/RiderMain" element={<RiderMain />} />
             <Route path="/RiderCall" element={<RiderCall />} />
             <Route path="/RiderOrder" element={<RiderOrder />} />
-            <Route path="/RiderOrderList" element={<RiderOrderList />} />
+            {/* <Route path="/RiderOrderList" element={<RiderOrderList />} /> */}
             <Route path="/RiderOrderReceipt" element={<RiderOrderReceipt />} />
-            <Route path="/RiderOrderReceiptList" element={<RiderOrderReceiptList />} />
-            <Route path="/RiderRechart" element={<RiderRechart />} />
+            {/* <Route path="/RiderOrderReceiptList" element={<RiderOrderReceiptList />} /> */}
+            {/* <Route path="/RiderRechart" element={<RiderRechart />} /> */}
             <Route path="/RiderRevenue" element={<RiderRevenue />} />
             <Route path="/RiderMyPage" element={<RiderMyPage />} />
             <Route path="/RiderMyPagemodify" element={<RiderMyPagemodify />} />
